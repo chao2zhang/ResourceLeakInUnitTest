377d5b41e7747652ad33bf28d0c3d1c405bbea76
Chao Zhang <cazhang@linkedin.com>
2021-08-25 15:55:03 -0700

    Initial commit

---
+++ usage/src/main/java/com/chao2zhang/usage/UseR.kt
@@ -0,0 +1,8 @@
+package com.chao2zhang.usage
+
+import com.chao2zhang.data.R
+
+class UseR {
+
+    val myResource = R.string.my_resource
+}
---
+++ usage/src/test/java/com/chao2zhang/usage/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package com.chao2zhang.usage
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import com.chao2zhang.data.R
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertNotEquals(R.string.my_resource, 0)
+    }
+}
